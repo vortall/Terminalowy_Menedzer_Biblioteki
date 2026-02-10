c5d31f40f09ae4057929222f2793017fd09801df
vortall <jaadrek@gmail.com>
2026-02-10 19:25:09 +0100

    Migracja UserRepository do bazy danych

--- src/main/java/pl/edu/wszib/biblioteka/database/UserRepository.java
+++ src/main/java/pl/edu/wszib/biblioteka/database/UserRepository.java
@@ -1,33 +1,63 @@
 package pl.edu.wszib.biblioteka.database;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.edu.wszib.biblioteka.configuration.DatabaseConfig;
 import pl.edu.wszib.biblioteka.model.Role;
 import pl.edu.wszib.biblioteka.model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class UserRepository implements IUserRepository {
-    private final List<User> users = new ArrayList<>();
-
-    public UserRepository() {
-        this.users.add(new User("admin", "21232f297a57a5a743894a0e4a801fc3", Role.ADMIN));
-        this.users.add(new User("user", "ee11cbb19052e40b07aac0ca060c23ee", Role.USER));
-    }
+    private final DatabaseConfig databaseConfig;
 
     @Override
     public User findUserByLogin(String login) {
-        for(User u : this.users) {
-            if(u.getUsername().equals(login)) {
-                return u;
+        String sql = "SELECT * FROM users WHERE username = ?";
+        try (Connection connection = databaseConfig.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            
+            statement.setString(1, login);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                return new User(
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        Role.valueOf(resultSet.getString("role"))
+                );
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
     @Override
     public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Connection connection = databaseConfig.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                users.add(new User(
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        Role.valueOf(resultSet.getString("role"))
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return users;
     }
 }
